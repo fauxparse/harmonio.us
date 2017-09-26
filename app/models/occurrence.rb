c28623a49fa7fa416ce5ece956073320186c503b
Matt Powell <fauxparse@gmail.com>
2017-09-27 08:31:22 +1300

    fix dodgy after_initialize hook

--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -2,6 +2,7 @@ class Occurrence < ApplicationRecord
   belongs_to :event, inverse_of: :existing_occurrences
 
   after_initialize :fill_in_ends_at, unless: :ends_at?
+  before_validation :fill_in_ends_at, unless: :ends_at?
 
   validates :starts_at, :ends_at, presence: true
   validates :starts_at, uniqueness: { scope: :event_id }
@@ -9,6 +10,6 @@ class Occurrence < ApplicationRecord
   private
 
   def fill_in_ends_at
-    self.ends_at = starts_at + event.duration
+    self.ends_at = starts_at + event.duration if event.present?
   end
 end
--- spec/models/occurrence_spec.rb
+++ spec/models/occurrence_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Occurrence, type: :model do
-  subject(:occurrence) { create(:occurrence) }
+  subject(:occurrence) { create(:occurrence, event: event) }
+  let(:event) { create(:event) }
 
   it { is_expected.to be_valid }
 end
